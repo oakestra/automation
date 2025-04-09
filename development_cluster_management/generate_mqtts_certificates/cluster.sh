8cb4a6fc53c9c614347184fd9c7c3b2f19e4eee6
Matthew Humphreys <ge65yay@tum.de>
2025-04-09 13:52:45 +0200

    updated mqtts scripts
    Signed-off-by: Matthew Humphreys <ge65yay@tum.de>

--- development_cluster_management/generate_mqtts_certificates/cluster.sh
+++ development_cluster_management/generate_mqtts_certificates/cluster.sh
@@ -1,6 +1,16 @@
+CA_CRT_P="${CA_CRT:-$CA_CRT_P}"
+CA_KEY_P="${CA_KEY:-$CA_KEY_P}"
+
+if [ ! -f $CA_CRT_P ]; then
+    echo "Generating Certificate Authority"
+    openssl req -new -x509 -days 365 -extensions v3_ca -keyout $CA_KEY_P -out $CA_CRT_P -subj "/C=DE/ST=Bavaria/L=Munich/O=Oakestra/OU=Certificate Authority"
+fi
+
 # MQTTS Broker
-echo "Generating Certificate Authority"
-openssl req -new -x509 -days 365 -extensions v3_ca -keyout ca.key -out ca.crt -subj "/C=DE/ST=Bavaria/L=Munich/O=Oakestra/OU=Certificate Authority"
+openssl genrsa -out server.key 2048
+echo "-------------------------------"
+
+# MQTTS Broker
 openssl genrsa -out server.key 2048
 echo "-------------------------------"
 
@@ -16,12 +26,17 @@ else
         echo "IPv6 detected - trimming [...]"
         openssl req -out server.csr -key server.key -new -addext "subjectAltName = IP:${ipv6}, DNS:mqtts" -subj "/C=DE/ST=Bavaria/L=Munich/O=Oakestra/OU=MQTTS/CN=mqtts"
     else
-        echo "DNS Detected"
-        openssl req -out server.csr -key server.key -new -addext "subjectAltName = DNS:${SYSTEM_MANAGER_URL}, DNS:mqtts" -subj "/C=DE/ST=Bavaria/L=Munich/O=Oakestra/OU=MQTTS/CN=mqtts"
+        if [[ -z "${SYSTEM_MANAGER_IP}" ]]; then
+            echo "DNS Detected"
+            openssl req -out server.csr -key server.key -new -addext "subjectAltName = DNS.1:${SYSTEM_MANAGER_URL} DNS.2:mqtts, IP:${SYSTEM_MANAGER_IP}" -subj "/C=DE/ST=Bavaria/L=Munich/O=Oakestra/OU=MQTTS/CN=mqtts"
+        else
+            echo "DNS and IP detected"
+            openssl req -out server.csr -key server.key -new -addext "subjectAltName = DNS.1:${SYSTEM_MANAGER_URL}, DNS.2:mqtts" -subj "/C=DE/ST=Bavaria/L=Munich/O=Oakestra/OU=MQTTS/CN=mqtts"
+        fi
     fi
 fi
 
-openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt -days 365 -copy_extensions copyall
+openssl x509 -req -in server.csr -CA $CA_CRT_P -CAkey $CA_KEY_P -CAcreateserial -out server.crt -days 365 -sha256 -extfile ~/ext.cnf
 echo "-------------------------------"
 chmod 0644 server.key   
 
@@ -31,7 +46,7 @@ openssl genrsa -aes256 -out cluster.key 2048
 echo "-------------------------------"
 openssl req -out cluster.csr -key cluster.key -new -subj "/C=DE/ST=Bavaria/L=Munich/O=Oakestra/OU=Cluster Manager/CN=cluster_manager"
 echo "-------------------------------"
-openssl x509 -req -in cluster.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out cluster.crt -days 365
+openssl x509 -req -in cluster.csr -CA $CA_CRT_P -CAkey $CA_KEY_P -CAcreateserial -out cluster.crt -days 365
 
 # Cluster Service Manager
 echo "Generating a key for the cluster service manager"
@@ -39,7 +54,7 @@ openssl genrsa -aes256 -out cluster_net.key 2048
 echo "-------------------------------"
 openssl req -out cluster_net.csr -key cluster_net.key -new -subj "/C=DE/ST=Bavaria/L=Munich/O=Oakestra/OU=Cluster service Manager/CN=cluster_service_manager"
 echo "-------------------------------"
-openssl x509 -req -in cluster_net.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out cluster_net.crt -days 365
+openssl x509 -req -in cluster_net.csr -CA $CA_CRT_P -CAkey $CA_KEY_P -CAcreateserial -out cluster_net.crt -days 365
 
 
 echo "###############################"
@@ -49,4 +64,3 @@ echo "export CLUSTER_KEYFILE_PASSWORD=<cluster keyfile password>"
 echo "export CLUSTER_SERVICE_KEYFILE_PASSWORD=<cluster_net keyfile password>"
 echo "###############################"
 echo "###############################"
-
